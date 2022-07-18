8f868a159dc48b9b24b907376145f5d8e9c50e6a
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-07-18 14:47:50 +0100

    Create alertI.js

---
+++ alertI.js
@@ -0,0 +1,42 @@
+if(document.getElementById) {
+    window.alert = function(txt) {
+        alertI(txt);
+    }
+}
+
+function alertI(txt, title, button) {
+    d = document;
+
+    if(d.getElementById("modalContainer")) return;
+
+    mObj = d.getElementsByTagName("body")[0].appendChild(d.createElement("div"));
+    mObj.id = "modalContainer";
+    mObj.style.height = d.documentElement.scrollHeight + "px";
+
+    alertObj = mObj.appendChild(d.createElement("div"));
+    alertObj.id = "alertBox";
+    if(d.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
+    alertObj.style.left = (d.documentElement.scrollWidth - alertObj.offsetWidth)/2 + "px";
+    alertObj.style.visiblity="visible";
+
+    h1 = alertObj.appendChild(d.createElement("h1"));
+    h1.appendChild(d.createTextNode(title));
+
+    msg = alertObj.appendChild(d.createElement("p"));
+    //msg.appendChild(d.createTextNode(txt));
+    msg.innerHTML = txt;
+
+    btn = alertObj.appendChild(d.createElement("a"));
+    btn.id = "closeBtn";
+    btn.appendChild(d.createTextNode(button));
+    btn.href = "#";
+    btn.focus();
+    btn.onclick = function() { removeCustomAlert();return false; }
+
+    alertObj.style.display = "block";
+
+}
+
+function alertIRemove() {
+    document.getElementsByTagName("body")[0].removeChild(document.getElementById("modalContainer"));
+}
